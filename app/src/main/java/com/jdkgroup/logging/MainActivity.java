6e26ad8f847d6d6eb7e68b8440427dbf2bd97e2a
kamlesh <kamal.lakhani56@gmail.com>
2017-08-03 09:07:08 -0700

    ignore .idea

--- app/src/main/java/com/jdkgroup/logging/MainActivity.java
+++ app/src/main/java/com/jdkgroup/logging/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Debug.log("Tag", "Welcome to  android");
+        Debug.log("Tag", "Welcome to  android");  
     }
 }
