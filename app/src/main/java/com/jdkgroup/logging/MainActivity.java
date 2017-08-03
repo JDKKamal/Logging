78b01e98442764cab3c5342b37e8ce0ebfe741cd
kamlesh <kamal.lakhani56@gmail.com>
2017-08-03 09:01:20 -0700

    Initial Commint

---
+++ app/src/main/java/com/jdkgroup/logging/MainActivity.java
@@ -0,0 +1,17 @@
+package com.jdkgroup.logging;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jdkgroup.utils.Debug;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Debug.log("Tag", "Welcome to  android");
+    }
+}
